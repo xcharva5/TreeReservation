6029f0161705fd53b516aee06905176a281fdd2c
ondrej.charvat <ondrej.charvat@logex.nl>
2020-12-09 17:51:39 +0100

    0.0.1
    removed previous authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,11 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
-// Auth service
-import { AuthService } from './shared/services/auth.service';
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { TreeComponent } from './components/reservations/tree/tree.component';
 import { TreeDetailsComponent } from './components/reservations/tree-details/tree-details.component';
-import { SignInComponent } from './components/auth/sign-in/sign-in.component';
-import { SignUpComponent } from './components/auth/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './components/auth/verify-email/verify-email.component';
 
 @NgModule({
   declarations: [
@@ -28,10 +21,6 @@ import { VerifyEmailComponent } from './components/auth/verify-email/verify-emai
     TreeDetailsComponent,
     AboutComponent,
     ReservationsComponent,
-    SignInComponent,
-    SignUpComponent,
-    ForgotPasswordComponent,
-    VerifyEmailComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,9 +29,7 @@ import { VerifyEmailComponent } from './components/auth/verify-email/verify-emai
     AngularFireAuthModule,
     AngularFirestoreModule,
   ],
-  providers: [
-    AuthService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
